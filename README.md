4e9e039d6eeebd3f9aa0cd96fff6e0a189d7353c
azhilin <azhilin@technotimes.org>
2020-06-30 17:37:25 +0300

    Initial commit
    Add scan_interval option

---
+++ README.md
@@ -0,0 +1,37 @@
+# Yandex Rain Sensor
+
+_Простой сенсор, который показывает текстом информацию о текущих или грядущих осадках_
+
+![example][example1]
+
+## Установка
+
+Скопировать содержимое директории `/custom_components/yandex_rain/` в `<hass-config>/custom_components/yandex_rain/`.
+
+Или импортировать как кастомный репозиторий в [HACS](https://hacs.xyz/docs/faq/custom_repositories/).
+
+
+## Конфигурация
+  
+key | type | description  
+:--- | :--- | :---  
+**platform (Required)** | string | Платформа сенсора.
+**name (Required)** | string | Название сенсора в Home Assistant.
+**latitude (Optional)** | float | Широта. По умолчанию берется из конфигурации Home Assistant
+**longitude (Optional)** | float | Долгота. По умолчанию берется из конфигурации Home Assistant
+**scan_interval (Optional)** | int | Интервал обновления в секундах. По умолчанию 300 секунд (5 минут)
+
+
+## Пример конфигурации в configuration.yaml
+
+```yaml
+sensor:
+  platform: yandex_maps
+  name: rain_info
+  latitude: 12.34
+  longitude: 23.45
+  scan_interval: 300
+```
+
+
+[example1]: images/example1.png
---
+++ custom_components/yandex_rain/sensor.py
@@ -0,0 +1,101 @@
+import aiohttp
+import datetime
+import homeassistant.helpers.config_validation as cv
+import logging
+import time
+import voluptuous as vol
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.const import (
+    CONF_LATITUDE,
+    CONF_LONGITUDE,
+    CONF_NAME,
+    CONF_SCAN_INTERVAL,
+)
+from homeassistant.helpers.entity import Entity
+
+
+__version__ = '0.1.0'
+
+_LOGGER = logging.getLogger(__name__)
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_NAME): cv.string,
+    vol.Optional(CONF_LATITUDE): vol.Coerce(float),
+    vol.Optional(CONF_LONGITUDE): vol.Coerce(float),
+    vol.Optional(CONF_SCAN_INTERVAL, default=300): cv.time_period,
+})
+
+BASE_URL = 'https://yandex.ru/pogoda/front/maps/nowcast-single?lat={lat}&lon={lon}&lang=ru&alert=1'
+
+SUPPORTED_ALERT_TYPES = {'rain', 'noprec'}
+
+ATTR_LAST_UPDATE = 'last_update'
+ATTR_RAIN_STATE = 'rain_state'
+ATTR_RAIN_STATE_LIST = 'rain_state_list'
+
+RAIN_STATES = ['starts', 'ends', 'noprec', 'still']
+
+
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    name = config[CONF_NAME]
+    lat = config.get(CONF_LATITUDE, hass.config.latitude)
+    lon = config.get(CONF_LONGITUDE, hass.config.longitude)
+
+    async_add_entities([YandexRainSensor(hass, name, lat, lon)], True)
+
+
+class YandexRainSensor(Entity):
+    def __init__(self, hass, name, lat, lon, scan_interval):
+        self.hass = hass
+        self._state = None
+        self._name = name
+        self._lat = lat
+        self._lon = lon
+        self._scan_interval = scan_interval
+        self.attr = {
+            ATTR_RAIN_STATE_LIST: RAIN_STATES
+        }
+        self._update_ts = time.monotonic()
+        _LOGGER.debug(f'Initialized sensor {self._name}')
+
+    async def async_update(self):
+        _LOGGER.debug(f'Updating sensor {self._name}...')
+        if (time.monotonic() - self._update_ts) >= self._scan_interval:
+            try:
+                url = BASE_URL.format(lat=self._lat, lon=self._lon)
+                async with aiohttp.ClientSession() as client:
+                    async with client.get(url) as resp:
+                        response = await resp.json()
+                        alert = response['alert']
+                        _LOGGER.debug(f'Got alert response: {alert}')
+
+                        if alert['type'] not in SUPPORTED_ALERT_TYPES:
+                            _LOGGER.warning(f"Got unsupported alert type: {alert['type']}")
+                            return
+
+                        self._state = alert.get('title', None)
+
+                        self.attr[ATTR_RAIN_STATE] = alert.get('state', None)
+                        updated_ts = alert.get('time')
+                        if updated_ts is not None:
+                            self.attr[ATTR_LAST_UPDATE] = datetime.datetime.fromtimestamp(updated_ts)
+            except Exception as ex:
+                _LOGGER.error(f'Could not update {self._name}: {ex}')
+        else:
+            _LOGGER.debug('Skipping update')
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def state(self):
+        return self._state
+
+    @property
+    def icon(self):
+        return 'mdi:weather-pouring'
+
+    @property
+    def device_state_attributes(self):
+        return self.attr
